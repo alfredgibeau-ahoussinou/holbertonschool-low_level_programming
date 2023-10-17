bf2aa1bdd2e3adb630aea142d187a65d770a48bb
alfredgibeau-ahoussinou <alfredgibeauahoussinou@gmail.com>
2023-10-17 03:50:12 -0700

    CP2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (if success)
+ */
+
+int main(void)
+{
+
+	char lettre;
+
+	for (lettre = 'a' ; lettre <= 'z' ; lettre++)
+	putchar(lettre);
+	putchar('\n');
+	return (0);
+}
