d58db864b07474a9758b1fcb5bf2bc79ff202c58
alfredgibeau-ahoussinou <alfredgibeauahoussinou@gmail.com>
2023-10-19 04:34:38 -0700

    CP2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *print_alphabet_x10 prints 10 times the alphabet, in lowercase followed by a new line.
+ *print_alphabet_x10 prints 10 times the alphabet
  */
 
 void print_alphabet_x10(void)
