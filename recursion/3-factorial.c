e04dbdad80fe5c8cc55a241aa27399f051922419
alfredgibeau-ahoussinou <7550@holbertonstudents.com>
2023-11-01 01:53:54 -0700

    CP2

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -3,17 +3,22 @@
 
 /*
  * factorial - Write a function that returns the factorial of a given number.
- * @n
+ * 
+ * @n: This is the input number
+ *
+ * Return: The factorial of a given number
  */
 
 int factorial(int n)
-{	
 {
-	if (n == 0 || n == 1)
-	return 1;
+	if (n == 0)
+	{
+	return (1);
+	}
+	if (n < 0)
+	{
+	return (-1);
+	}
+	return (n * factorial(n - 1));
 }
-else 
-{
-	return n * factorial(n + 1)
-}
-}
+
