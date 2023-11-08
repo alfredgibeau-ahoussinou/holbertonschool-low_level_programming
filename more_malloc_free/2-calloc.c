e0f24aef1b67fe7be4aa94845c887774dba2a990
alfredgibeau-ahoussinou <7550@holbertonstudents.com>
2023-11-08 01:46:31 -0800

    CAE

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,27 +1,24 @@
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * _calloc - Allocate memory for array of nmemb elements of size bytes
- * @nmemb: Number of elemnts
- * @size: Size in bytes of elements
+ * _calloc - function
+ * @nmemb: unsigned int
+ * @size: unsigned int
  *
- * Return: Pointer to new memory, NULL if it fails
+ * Return: int
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+void	*_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ar;
-	unsigned int ar_size, i;
+	void		*r;
+	unsigned int	x;
 
 	if (nmemb == 0 || size == 0)
-		return (NULL);
-	ar_size = nmemb * size;
-	ar = malloc(ar_size);
-	if (ar == NULL)
-		return (NULL);
-	while (i < ar_size)
-	{
-		ar[i] = 0;
-		i++;
-	}
-	return (ar);
+		return (0);
+	r = malloc(nmemb * size);
+	if (r == 0)
+		return (0);
+	for (x = 0; x < (nmemb * size); x++)
+		((unsigned char *) r)[x] = (unsigned char) 0;
+	return (r);
 }
