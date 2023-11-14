a4b8ff36a1b0a96bf5ef91eadbe43b25ec1903e8
alfredgibeau-ahoussinou <7550@holbertonstudents.com>
2023-11-14 01:23:53 -0800

    CP0

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free dog - free a dog
+ *
+ * @d:day to free
+ *
+ * Return: void
+ **/
+
+void free_dog(dog_t *d)
+
+{
+	if (d == 0)
+		return;
+	if  (d -> name)
+		free (d -> name);
+	if (d -> owner)
+		free (d -> owner);
+	free(d);
+}
+
