773518da8719c984c87efb618828ee3e72fa1d48
alfredgibeau-ahoussinou <7550@holbertonstudents.com>
2023-11-15 01:21:22 -0800

    CAP7

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -13,13 +13,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-  if (d == NULL)
-    ;
-  else
-    {
-      d->name = name;
-      d->age = age;
-      d->owner = owner;
-    }
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
 
